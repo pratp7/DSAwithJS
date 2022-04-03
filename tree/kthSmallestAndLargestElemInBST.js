1ea248d8dd2718604d7ab7c2f54a6dcb4952a36f
Pratyush Prasar <69028293+pratp7@users.noreply.github.com>
2022-04-04 00:21:17 +0530

    Update and rename kthSmallestElemInBST.js to kthSmallestAndLargestElemInBST.js

--- tree/kthSmallestAndLargestElemInBST.js
+++ tree/kthSmallestAndLargestElemInBST.js
@@ -13,3 +13,18 @@ function kthSmallestElementInABst(root, k) {
     
     return arr[k-1];
 }
+
+
+
+function kthLargestElementInABst(root, k) {
+let arr = [];
+inorder(root);
+return arr[arr.length-k];
+
+function inorder(root){
+    if(!root) return null;
+    if(root.left) inorder(root.left);
+    arr.push(root.val);
+    if(root.right) inorder(root.right);
+}
+}
